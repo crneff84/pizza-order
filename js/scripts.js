7ddb471fb2bbf8ca026e874686a341886b61439f
Chance Neff <crneff84@gmail.com>
2016-08-26 11:21:07 -0700

    checkboxes working

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 //Back End
-function Pizza (size, topping) {
+function Pizza (size, sauce, topping) {
   this.size = size;
+  this.sauce = sauce;
   this.topping = topping;
 }
 
-Pizza.prototype.pizzaPricer = function(size, topping){
-  var price = 10;
+Pizza.prototype.pizzaPricer = function(size, sauce, topping){
+  var price = 0;
   if (size === NaN) {
     alert("Please Choose a Pizza Size");
   } else if (size === 2) {
-    price +=2;
+    price +=12;
   } else if (size === 3){
-    price += 4;
+    price += 14;
   } else if (size === 4) {
-    price += 6;
+    price += 16;
   }
   if (topping === "1") {
     price += 1;
@@ -32,9 +33,13 @@ $(document).ready(function(){
   $("form#pizza-order").submit(function(event){
     event.preventDefault();
     var sizeInput = parseInt($("input:radio[name=size]:checked").val());
-    var toppingInput = $("input:radio[name=topping]:checked").val();
-    var thePizza = new Pizza(sizeInput, toppingInput);
-    var finalPrice = thePizza.pizzaPricer(sizeInput, toppingInput);
+    var sauceInput = parseInt($("input:radio[name=sauce]:checked").val());
+    var toppingSum = 0;
+    $("input:checkbox:checked").each(function(){
+      toppingSum += parseInt($(this).val());
+    });
+    var thePizza = new Pizza(sizeInput, toppingSum);
+    var finalPrice = thePizza.pizzaPricer(sizeInput, sauceInput, toppingSum);
     $("#price-output").text(finalPrice);
     $("#display-output").show();
 
